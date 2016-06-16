960bdc7e8dca06b4985e143a655d9559dbafe4a4
JMwill <787505998@qq.com>
2016-06-16 21:57:06 +0800

    学习单页应用开发

---
+++ summary/Algorithmetic-summary/leetcode.com/problem-1-50/problem-6.js
@@ -0,0 +1,19 @@
+/**
+ * 将给定字符串以“之字形”模式，写成给定的行数，如下所示是由"PAYPALISHIRING"而得：
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ *
+ * 即为："PAHNAPLSIIGYIR"
+ *
+ * 思路：每一行字之间相差（行数-2）个空间，最后一行除外。因此，每一行的
+ */
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+
+};
