6f8820991b0111ebc7150fdbc672121dbef7633c
JMwill <787505998@qq.com>
2016-08-03 19:02:12 +0800

    修改

--- summary/front-end-summary/JavaScript-Application-Design/practice/meizi_spider/spider/meizi/index.js
+++ summary/front-end-summary/JavaScript-Application-Design/practice/meizi_spider/spider/meizi/index.js
@@ -6,13 +6,16 @@ function Spider(option) {
     this.spiderInterval;
     this.urlQueue = [];
     this.imgQueue = [];
+    this.options = {
+        timeout: 1000
+    };
 }
 
 Spider.prototype.stop = function() {
     clearInterval(spiderInterval);
 }
 
-Spider.prototype.spide = function(url) {
+Spider.prototype.crawl = function(url) {
     crawler.crawl(url, ['--ignore-ssl-errors=yes', '--load-images=no']);
 }
 
@@ -30,21 +33,37 @@ Spider.prototype.queuePush = function (url) {
 Spider.prototype.infinitySpide = function () {
     let spide = function () {
         let url = this.urlQueue.pop();
-        this.spide(url);
+        if (!url) {return;}
+        this.crawl(url);
     };
     spide = spide.bind(this);
-    spiderInterval = setInterval(spide, timeout);
+
+    clearInterval(this.spiderInterval);
+    this.spiderInterval = setInterval(spide, timeout);
+}
+
+Spider.prototype.randomSpide = function (second) {
+    second = parseInt(second);
+    if (isNaN(second)) {return};
+
+    let time = Math.floor(Math.random() * second);
+
+    clearTimeout(this.spiderInterval);
+    this.spiderInterval = setTimeout((function {
+        let url = this.urlQueue.pop();
+        if (!url) {return;}
+        this.crawl(url);
+        this.randomSpide(second)
+        this.randomSpide(second);
+    }).bind(this), this.options.timeout);
 }
 
 function wrapper(entryUrl, options) {
     let spider = new Spider(options);
     if (options) {
-        spider.options = {
-            timeout: 1000
-        };
         Object.assign(spider.options, options);
     }
-    spider.queuePush
+    spider.queuePush(entryUrl);
     return spider;
 }
 
