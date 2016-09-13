47b0afda0d79b97e25cc1789fdc14179b90e5686
JMwill <787505998@qq.com>
2016-09-13 14:52:16 +0800

    消除代码异味，合并重复代码

--- summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/functional_tests.py
+++ summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 访问应用首页
         self.browser.get('http://localhost:8000')
@@ -36,6 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # 用户按下回车键后，页面更新了
         # 待办事项表格中显示“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 用户输入了“Use peacock feathers to make fly”（使用孔雀羽毛做假蝇）
@@ -46,19 +52,12 @@ class NewVisitorTest(unittest.TestCase):
         # 页面再次更新，用户清单上显示两个待办事项
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows]
-        )
-        self.assertIn(
-            '2: Use peacock feathers to make fly',
-            [row.text for row in rows]
-        )
-
-        self.fail('Finish the test!')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
         # 用户想知道网站是否会记住清单内容
         # 用户看到网站为他生成一个文字解说功能
+        self.fail('Finish the test!')
 
         # 用户访问那个URL，发现待办事项列表还在
 
