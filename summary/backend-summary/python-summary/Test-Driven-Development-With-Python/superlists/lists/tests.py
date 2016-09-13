8067cdc9f37b1ecaca6ec7495b0cb3ec1da08d9f
JMwill <787505998@qq.com>
2016-09-13 16:45:28 +0800

    通过测试

--- summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/lists/tests.py
+++ summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/lists/tests.py
@@ -7,6 +7,7 @@ from lists.views import home_page
 from lists.models import Item
 
 
+# Create your tests here.
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -24,7 +25,8 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
-# Create your tests here.
+
+
 class HomePageTest(TestCase):
     """Test Home page activity"""
     def test_root_url_resolves_to_home_page_view(self):
@@ -43,9 +45,31 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
--- summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/lists/views.py
+++ summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/lists/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        return render(
-            request,
-            'home.html',
-            {'new_item_text': request.POST.get('item_text', '')}
-        )
-    return render(request, 'home.html')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
