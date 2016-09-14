ec361aca6fff29d9b7fabc5561e5385017a0bcce
JMwill <787505998@qq.com>
2016-09-14 16:32:19 +0800

    修改功能测试

--- summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/functional_tests/tests.py
+++ summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/functional_tests/tests.py
@@ -39,9 +39,11 @@ class NewVisitorTest(LiveServerTestCase):
         # 用户的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        # 用户按下回车键后，页面更新了
-        # 待办事项表格中显示“1: Buy peacock feathers”
+        # 用户按下回车键后，被带到一个新URL
+        # 这个页面的待办事项表格中显示“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
@@ -51,15 +53,34 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，用户清单上显示两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
-        # 用户想知道网站是否会记住清单内容
-        # 用户看到网站为他生成一个文字解说功能
-        self.fail('Finish the test!')
+        ## 我们使用一个新浏览器回话
+        ## 确保最开始的用户的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # 用户访问那个URL，发现待办事项列表还在
+        # 现在，另一个用户（Tom）访问首页
+        # 页面在中看不到前一个用户的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Tom输入一个新待办事项，新建一个清单
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Tom获得了他的唯一URL
+        tom_list_url = self.browser.current_url
+        self.assertRegex(tom_list_url, '/lists/.+')
+        self.assertNotEqual(tom_list_url, edith_list_url)
+
+        # 这个页面还是没有最开始的用户的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # 用户结束查看网站，退出了网站
