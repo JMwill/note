ba68a9bb8875c0f418b4db2e00d2fc4f38c8c20e
JMwill <787505998@qq.com>
2016-09-18 16:23:43 +0800

    修改路由

--- summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/lists/urls.py
+++ summary/backend-summary/python-summary/Test-Driven-Development-With-Python/superlists/lists/urls.py
@@ -12,11 +12,10 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
 ]
