f0100a431a76724100900652cbb3639fe6e12222
JMwill <787505998@qq.com>
2016-10-07 11:26:26 +0800

    添加

--- summary/backend-summary/python-summary/python-sicp/Chapter-3/exec2.py
+++ summary/backend-summary/python-summary/python-sicp/Chapter-3/exec2.py
@@ -4,4 +4,10 @@ def test_search():
 	dotstar = star(dot)
 	assert search(lit('def'), 'abcdefg') == 'def'
 	assert search(seq(lit('def'), eol), 'abcdef') == 'def'
-	
+	assert search(seq(lit('def'), eol), 'abcdefg') == None
+	assert search(a, 'not the start') == 'a'
+	assert match(a, 'not the start') == None
+	assert match(abcstars, 'aaabbbccccccccdef') == 'aaabbbcccccccc'
+	assert match(abcstars, 'junk') == ''
+	assert all(match(seq(abcstars, eol), s) == s
+					for s in 'abc aaabbccc aaaabcccc'.split())
