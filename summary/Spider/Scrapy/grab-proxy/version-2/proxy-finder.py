5483798d197503ee6544807b707008bf28c5e3e2
JMwill <787505998@qq.com>
2016-10-11 21:10:35 +0800

    ./

--- summary/Spider/Scrapy/grab-proxy/version-2/proxy-finder.py
+++ summary/Spider/Scrapy/grab-proxy/version-2/proxy-finder.py
@@ -16,4 +16,19 @@ from BeautifulSoup import BeautifulSoup
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument
+    parser.add_argument('-s', '--show', help='Show this number of results. Example: "-s 5" will show the 5 fastest proxies then stop')
+    parser.add_argument('-a', '--all', help='Show all proxy results including the ones that failed 1 of the 3 tests', action='store_true')
+    parser.add_argument('-q', '--quiet', help='Only print the IP:port of the fastest proxies that pass all the tests', action='store_true')
+    return parser.parse_args()
+
+
+class FindHttpProxy():
+    ''' Only gather L1 proxies which should not give out IP
+    or advertise that you are using a proxy at all '''
+    
+    def __init__(self, args):
+        self.proxy_list = []
+        self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36'}
+        self.show_num = args.show
+        self.show_all = args.all
+        
