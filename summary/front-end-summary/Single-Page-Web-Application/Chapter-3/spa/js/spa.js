33f2a887843311916f43498bcf9a0447cfbf5a6f
JMwill <787505998@qq.com>
2016-10-21 12:29:05 +0800

    开发chat模块

--- summary/front-end-summary/Single-Page-Web-Application/Chapter-3/spa/js/spa.js
+++ summary/front-end-summary/Single-Page-Web-Application/Chapter-3/spa/js/spa.js
@@ -12,11 +12,7 @@
 /* global $, spa */
 var spa = (function() {
 	var initModule = function ($container) {
-		$container.html(
-			'<h1 style="display:inline-block; margin: 25px;">' +
-				'hello world!' +
-			'</h1>'
-		);
+        spa.shell.initModule($container)
 	};
 
 	return { initModule: initModule };
