7d5900390a0be51cd2e575dab6d5e7c9450ee57a
JMwill <787505998@qq.com>
2016-12-01 13:44:44 +0800

    学习Ninja第八章

---
+++ summary/front-end-summary/Vue/learn-proj-1/src/bs-themes-one/src/components/Container/ThreeColContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <slot name="left"></slot>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <slot name="middle"></slot>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <slot name="right"></slot>
+            </div>
+        </div>
+    </div>
+</template>
