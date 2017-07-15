dd0d800e9c688e7738bbe7c157daee93c32b24dd
JMwill <787505998@qq.com>
2017-07-15 11:32:04 +0800

    抽象出选择note的方法

--- Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-2/note_book/notebook.py
+++ Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-2/note_book/notebook.py
@@ -39,15 +39,23 @@ class Notebook:
         '''Create a new note and add it to the list.'''
         self.notes.append(Note(memo, tags))
 
+    def _find_note(self, note_id):
+        '''Locate the note with the given id.'''
+        # realization of book
+        # for note in slef.notes:
+        #     if note.id == note_id:
+        #         return note
+        # return None
+        return next((n for n in self.notes if n.id == note_id), None)
+
     def modify_memo(self, note_id, memo):
         '''Find the note with the given id and change its
         memo to the given value.'''
         # realization of book
-        # for note in self.notes:
-        #     if note.id == note_id:
-        #         note.memo = memo
-        #         break
-        note = next((n for n in self.notes if n.id == note_id), None)
+        # self._find_note(note_id).memo = memo;
+
+        # more safe
+        note = self._find_note(note_id)
         if isinstance(note, Note):
             note.memo = memo
 
@@ -55,11 +63,10 @@ class Notebook:
         '''Find the notes with the given id and change its
         tags to the given value.'''
         # realization of book
-        # for note in self.notes:
-        #     if note.id == note_id:
-        #         note.tags = tags
-        #         break
-        note = next((n for n in self.notes if n.id == note_id), None)
+        # self._find_note(note_id).tags = tags;
+
+        # more safe
+        note = self._find_note(note_id)
         if isinstance(note, Note):
             note.tags = tags
 
