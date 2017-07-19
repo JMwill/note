d09c0c51ad41a4eed15b4a4c7c3bce7672b59b8f
JMwill <787505998@qq.com>
2017-07-19 21:03:56 +0800

    修正代码错误,并将验证输入代码抽取成一个函数来调用

--- need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
+++ need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# 验证函数实现
+def get_valid_input(input_string, valid_options):
+    input_string += '({}) '.format(', '.join(valid_options))
+    response = input(input_string)
+    while response.lower() not in valid_options:
+        response = input(input_string)
+    return response
+
 class Property:
 
     def __init__(self, square_feet='', beds='', baths='', **kwargs):
@@ -40,20 +48,17 @@ class Apartment (Property):
         print('laundry: %s' % self.laundry)
         print('has balcony: %s' % self.balcony)
 
+    @staticmethod
+    def prompt_init():
         parent_init = Property.prompt_init()
-        laundry = ''
-        while laundry.lower() not in Apartment.valid_laundries:
-            laundry = input('What laundry facilities does '
-                            'the property have? ({})'.format(
-                                ', '.join(Apartment.valid_laundries)
-                            ))
-        balcony = ''
+        laundry = get_valid_input(
+            'What laundry facilities does '
+            'the property have?',
+            Apartment.valid_laundries)
 
-        while balcony.lower() not in Apartment.valid_balconies:
-            balcony = input(
-                'Does the property have a balcony? '
-                '({})'.format(
-                    ', '.join(Apartment.valid_balconies)))
+        balcony = get_valid_input(
+            'Does the property have a balcony? ',
+            Apartment.valid_balconies)
 
         parent_init.update({
             'laundry': laundry,
@@ -61,4 +66,4 @@ class Apartment (Property):
         })
         return parent_init
 
-    prompt_init = staticmethod(prompt_init)
+    # prompt_init = staticmethod(prompt_init)
