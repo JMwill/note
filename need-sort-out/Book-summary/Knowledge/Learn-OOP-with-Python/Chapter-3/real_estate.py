964a670dfccd9947ae9bef032a6827d2830f36c0
JMwill <787505998@qq.com>
2017-07-20 20:41:15 +0800

    添加ApartmentRental类

--- need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
+++ need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
@@ -149,6 +149,14 @@ class HouseRental (Rental, House):
         return init
     # prompt_init = staticmethod(prompt_init)
 
+class ApartmentRental (Rental, Apartment):
+    @staticmethod
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
+
 if __name__ == '__main__':
     init = HouseRental.prompt_init()
     house = HouseRental(**init)
