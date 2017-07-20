c27aa12c15ba2a3df85910a199f56800d390b7ca
JMwill <787505998@qq.com>
2017-07-20 20:10:34 +0800

    添加Rental类

--- need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
+++ need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
@@ -84,3 +84,27 @@ class Purchase:
             price=input('What is the selling price? '),
             taxes=input('What are the estimated taxes? '))
     # prompt_init = staticmethod(prompt_init)
+
+class Rental:
+    def __init__(self, furnished='', utilities='', rent='', **kwargs):
+        super().__init__(**kwargs)
+        self.furnished = furnished
+        self.rent = rent
+        self.utilities = utilities
+
+    def display(self):
+        super.display()
+        print('RENTAL DETAILS')
+        print('rent: {}'.format(self.rent))
+        print('estimated utilities: {}'.format(self.utilities))
+        print('furnished: {}'.format(self.furnished))
+
+    @staticmethod
+    def prompt_init():
+        return dict(
+            rent=input('What is the monthly rent? '),
+            utilities=input('What are the estimated utilities? '),
+            furnished=get_valid_input(
+                'Is the property furnished? ',
+                ('yes', 'no')))
+    # prompt_init = staticmethod(prompt_init)
