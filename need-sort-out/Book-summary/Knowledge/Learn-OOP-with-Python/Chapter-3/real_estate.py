74b4a0e344db08aa44189031986319a75ab9eaaf
JMwill <787505998@qq.com>
2017-07-20 20:25:19 +0800

    补充House类

--- need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
+++ need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-3/real_estate.py
@@ -66,6 +66,37 @@ class Apartment (Property):
         return parent_init
     # prompt_init = staticmethod(prompt_init)
 
+class House (Property):
+    valid_garage = ('attached', 'detached', 'none')
+    valid_fenced = ('yes', 'no')
+
+    def __init__(self, num_stories='', garage='', fenced='', **kwargs):
+        super().__init__(**kwargs)
+        self.garage = garage
+        self.fenced = fenced
+        self.num_stories = num_stories
+
+    def display(self):
+        super().display()
+        print('HOUSE DETAILS')
+        print('# of stories: {}'.format(self.num_stories))
+        print('garages: {}'.format(self.garage))
+        print('fenced yard: {}'.format(self.fenced))
+
+    @staticmethod
+    def prompt_init():
+        parent_init = Property.prompt_init()
+        fenced = get_valid_input('Is the yard fenced? ', House.valid_fenced)
+        garage = get_valid_input('Is there a garage? ', House.valid_garage)
+        num_stories = input('How many stories? ')
+        parent_init.update({
+            'fenced': fenced,
+            'garage': garage,
+            'num_stories': num_stories
+        })
+        return parent_init
+    # prompt_init = staticmethod(prompt_init)
+
 class Purchase:
     def __init__(self, price='', taxes='', **kwargs):
         super().__init__(**kwargs)
@@ -73,7 +104,7 @@ class Purchase:
         self.taxes = taxes
 
     def display(self):
-        super.display()
+        super().display()
         print('PURCHASE DETAILS')
         print('selling price: {}'.format(self.price))
         print('estimated taxes: {}'.format(self.taxes))
@@ -93,7 +124,7 @@ class Rental:
         self.utilities = utilities
 
     def display(self):
-        super.display()
+        super().display()
         print('RENTAL DETAILS')
         print('rent: {}'.format(self.rent))
         print('estimated utilities: {}'.format(self.utilities))
@@ -107,4 +138,12 @@ class Rental:
             furnished=get_valid_input(
                 'Is the property furnished? ',
                 ('yes', 'no')))
+    # prompt_init = staticmethod(prompt_init)
+
+class HouseRental (Rental, House):
+    @staticmethod
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
     # prompt_init = staticmethod(prompt_init)
