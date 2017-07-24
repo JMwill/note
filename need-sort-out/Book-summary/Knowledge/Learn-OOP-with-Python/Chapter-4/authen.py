bf4dce1d0b3f3fd51bd3477730222303c169c9f2
JMwill <787505998@qq.com>
2017-07-24 18:59:20 +0800

    添加登录方法

--- need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-4/authen.py
+++ need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-4/authen.py
@@ -60,3 +60,15 @@ class Authenticator:
         if len(password) < 6:
             raise PasswordTooShort(username)
         self.users[username] = User(username, password)
+
+    def login(self, username, password):
+        try:
+            user = self.users[username]
+        except KeyError:
+            raise InvalidUsername(username)
+
+        if not user.check_password(password):
+            raise InvalidPassword(username, user)
+
+        user.is_logged_in = True
+        return True
