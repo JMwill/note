a62472ac3f938f8d62ebdd296b430d829d6a4e37
JMwill <787505998@qq.com>
2017-07-24 15:43:53 +0800

    创建需要的异常类

--- need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-4/authen.py
+++ need-sort-out/Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-4/authen.py
@@ -23,3 +23,18 @@ class User:
         user, false otherwise.'''
         encrypted = self._encrypt_pw(password)
         return encrypted == self.password
+
+
+class AuthException(Exception):
+    def __init__(self, username, user=None):
+        super().__init__(username, user)
+        self.username = username
+        self.user = user
+
+
+class UsernameAlreadyExists(AuthException):
+    pass
+
+
+class PasswordTooShort(AuthException):
+    pass
