5b52f2a08a1e358571bd2a8e85b8d8921e12e46c
JMwill <787505998@qq.com>
2017-07-25 20:26:30 +0800

    初始化router以及services的引入

--- Boilerplate/front-boilerplate/vue-boilerplate/src/App.vue
+++ Boilerplate/front-boilerplate/vue-boilerplate/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <div id="app">
-    <app-header></app-header>
-    <router-view transition transition-mode="out-in"></router-view>
-  </div>
+    <div id="app">
+        <app-header></app-header>
+        <router-view transition transition-mode="out-in"></router-view>
+    </div>
 </template>
 
 <script>
 import AppHeader from './Header';
 
 export default {
-  name: 'app',
-  components: {
-    AppHeader,
-  },
+    name: 'app',
+    components: {
+        AppHeader,
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
 }
 </style>
--- Boilerplate/front-boilerplate/vue-boilerplate/src/main.js
+++ Boilerplate/front-boilerplate/vue-boilerplate/src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import store from 'store';
+import xhr from 'services';
+
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = xhr;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
+    el: '#app',
+    store,
+    router,
+    template: '<App/>',
+    components: { App },
 });
