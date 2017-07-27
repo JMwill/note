624972440b15ab20f38dbf5f7ba84dd08e048985
JMwill <787505998@qq.com>
2017-07-27 15:31:44 +0800

    改进gulpfile文件并添加实例文件目录结构

--- Boilerplate/front-boilerplate/vue-boilerplate/gulpfile.js
+++ Boilerplate/front-boilerplate/vue-boilerplate/gulpfile.js
@@ -7,98 +7,134 @@ const spritesmith = require('gulp.spritesmith');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 
+/**
+ * 路径变量定义区域
+ */
 const spritesPath = path.join(__dirname, 'src/assets/sprites');
+const spritesCssPath = path.resolve(spritesPath, '../css');
+const nospritesImgPath = path.resolve(spritesPath, '../img');
+const staticCssPath = path.join(__dirname, 'static/css');
+const staticImgPath = path.join(__dirname, 'static/img');
 
-let spritesArr = [];
-
-(function collectSpritesTask(dir) {
-    let filesList = [];
-
-    fs.readdirSync(dir).forEach((name) => {
-        const spritesFile = path.join(spritesPath, `${name}`);
-        const stats = fs.lstatSync(spritesFile);
-        if (stats.isFile() && /png^/.test(name)) {
-            filesList.push(spritesFile);
-        } else if (stats.isDirectory() && fs.readdirSync(spritesFile).length) {
-            const gulpTask = `sprites:${name}`;
-            spritesArr.push(gulpTask);
-            gulp.task(gulpTask, () => {
-                let spritesData =
-                    gulp.src(path.join(spritesFile, '*.png'))
-                        .pipe(spritesmith({
-                            imgName: `${name}_icon.png`,
-                            imgPath: `/img/${name}_icon.png`,
-                            cssName: `_${name}_icon.scss`,
-                        }));
-                spritesData.css
-                    .pipe(gulp.dest(path.join(__dirname, 'src/assets/css/')));
-
-                spritesData.img
-                    .pipe(buffer())
-                    .pipe(imagemin({
-                        optimizationLevel: 7, // 类型: Number, 默认: 3, 优化等级为: 0 ~ 7
-                        use: [pngquant()], // 这里使用 pngquant 深度压缩 png 图片的 imagemin 插件
-                    }))
-                    .pipe(gulp.dest(path.join(__dirname, 'static/img/')));
-            });
-        }
+/**
+ * Spritesmith 对象工厂, 用来给定名称来创建需要的 spritesmith对象
+ *
+ * @param {String} [name='/img']
+ * @returns {Object:spritesmith}
+ */
+function genSpritesmithObj(name, imgPath = '/img') {
+    return spritesmith({
+        imgName: `${name}_icon.png`,
+        imgPath: `${path.resolve(imgPath, `${name}_icon.png`)}`,
+        cssName: `_${name}_icon.scss`,
     });
+}
 
-    if (filesList.length) {
-        gulp.task('sprites:app', () => {
-            let spritesData =
-                gulp.src(path.join(spritesPath, '*.png'))
-                    .pipe(spritesmith({
-                        imgName: 'app_icon.png',
-                        imgPath: '/img/app_icon.png',
-                        cssName: '_app_icon.scss',
-                    }));
+/**
+ * Imagemin 对象工厂, 用来给定名称来创建需要的 imagemin对象
+ *
+ * @param {number} [level=7]
+ * @returns {Object:imagemin}
+ */
+function genImageminObj(opt) {
+    let finalOpt = Object.assign({}, {
+        optimizationLevel: 7, // 类型: Number, 默认: 3, 优化等级为: 0 ~ 7
+        use: [pngquant()], // 这里使用 pngquant 深度压缩 png 图片的 imagemin 插件
+    }, opt);
+    return imagemin(finalOpt);
+}
 
-            spritesData.css
-                .pipe(gulp.dest(path.join(__dirname, 'src/assets/css/')));
+/**
+ * 为雪碧图文件夹下的每个子文件夹定义一个任务
+ * 能够在某个子文件夹有修改的时候单独对其进行
+ * 修改
+ *
+ * @example 子文件夹生成的任务: sprites:子文件夹名
+ * @example 雪碧图根文件夹生成的任务: sprites:app
+ */
+(function collectSpritesTask(dir) {
+    // 获取雪碧图文件夹内部包含的文件/文件夹
+    let dirDetail = fs.readdirSync(dir);
 
-            spritesData.img
-                .pipe(buffer())
-                .pipe(imagemin({
-                    optimizationLevel: 7, // 类型: Number, 默认: 3, 优化等级为: 0 ~ 7
-                    use: [pngquant()], // 这里使用 pngquant 深度压缩 png 图片的 imagemin 插件
-                }))
-                .pipe(gulp.dest(path.join(__dirname, 'static/img/')));
+    // 获取所有包含文件的子文件夹, 不考虑递归
+    // 因为子文件内再有子文件夹的话组成上会变
+    // 复杂, 没有必要, 可以抽取到上一层
+    let subDirs = dirDetail.filter(
+        (d) => {
+            let subDirPath = path.resolve(dir, d);
+            return fs.lstatSync(subDirPath).isDirectory()
+                   && fs.readdirSync(subDirPath).length;
         });
-    }
+
+    // 将 sprites 根目录的任务名称跟 gulp 源对象对应上
+    let spritesData = {
+        app: gulp.src(path.resolve(dir, '*.png'))
+                .pipe(genSpritesmithObj('app')),
+    };
+
+    // 汇集所有的 sprites 任务
+    spritesData =
+    Object.assign.apply(null,
+        subDirs.map((d) => { // 将 sprites 子目录的任务名称跟 gulp 源对象对应上
+            let taskName = `sprites:${d}`;
+            let gulpObj = {};
+
+            gulpObj[taskName] =
+                gulp.src(path.resolve(dir, d, '*.png'))
+                    .pipe(genSpritesmithObj(d));
+            return gulpObj;
+        }).concat(spritesData)); // 添加将根目录的任务
+
+    // 定向所有 sprites 任务的 css 输出路径
+    // 压缩所有 sprites 任务的 png 图片, 并指定输出路径
+    Object.keys(spritesData)
+        .forEach((d) => {
+            spritesData[d].css
+                .pipe(gulp.dest(spritesCssPath));
+
+            spritesData[d].img
+                .pipe(buffer())
+                .pipe(genImageminObj())
+                .pipe(gulp.dest(staticImgPath));
+
+            gulp.task(d, () => spritesData[d]);
+        });
+
+    // 注册 sprites 任务, 其任务是运行所有子任务
+    gulp.task('sprites', Object.keys(spritesData), cb => cb());
 }(spritesPath));
 
 /**
- * scss:single 不依赖图片的合并
+ * 不执行 sprites 任务, 只单独对使用雪碧图
+ * 的 scss 文件进行处理并输出到静态文件夹下
  */
-gulp.task('scss:single', () => {
-    gulp.src(path.resolve(spritesPath, '../css/*.scss'))
+gulp.task('scss:only', () =>
+    gulp.src(path.resolve(spritesCssPath, '*.scss'))
         .pipe(sass({
             outputStyle: 'compressed',
         }).on('error', sass.logError))
-        .pipe(gulp.dest(path.join(__dirname, 'static/css/')));
-});
+        .pipe(gulp.dest(staticCssPath)));
 
-gulp.task('watch:scss', () => {
-    gulp.watch(path.resolve(__dirname, 'src/assets/css/*.scss', ['scss:single']));
-});
+/**
+ * 监听使用雪碧图的 scss 文件所在的文件夹
+ * 执行 scss:single 任务
+ */
+gulp.task('watch:scss', () => gulp.watch(path.resolve(spritesCssPath, '*.scss', ['scss:only'])));
 
-gulp.task('scss', ['sprites'], () => {
-    gulp.src(path.resolve(spritesPath, '../css/*.scss'))
-        .pipe(sass({
-            outputStyle: 'compressed',
-        }).on('error', sass.logError))
-        .pipe(gulp.dest(path.join(__dirname, 'static/css/')));
-});
+/**
+ * 定义 scss 任务, 需要等待 sprites 任务
+ * 执行完成, 然后再执行 scss:only 任务
+ */
+gulp.task('scss', ['sprites'], () => gulp.start('scss:only'));
 
-gulp.task('img', () => {
-    gulp.src(path.join(__dirname, 'src/assets/img/*.png'))
+/**
+ * 对资源文件夹下的 img 目录下的 png 文件
+ * 进行压缩处理并输出到静态文件夹下
+ */
+gulp.task('img', () =>
+    gulp.src(path.resolve(nospritesImgPath, '*.png'))
         .pipe(buffer())
-        .pipe(imagemin({
-            optimizationLevel: 7,
-            use: [pngquant()],
-        }))
-        .pipe(gulp.dest(path.join(__dirname, 'static/img/')));
-});
+        .pipe(genImageminObj())
+        .pipe(gulp.dest(staticImgPath)));
 
-gulp.task('default', ['sprites', 'scss', 'img']);
+gulp.task('default', ['img', 'sprites', 'scss']);
