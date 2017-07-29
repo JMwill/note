2360656516e2a9474657ceee472f407f4c2b01cb
JMwill <787505998@qq.com>
2017-07-29 10:23:34 +0800

    完成步骤执行方法的添加以及实现执行步骤的管理方法

--- Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-5/zip.py
+++ Book-summary/Knowledge/Learn-OOP-with-Python/Chapter-5/zip.py
@@ -23,3 +23,36 @@ class ZipReplace:
 
     def _full_filename(self, filename):
         return os.path.join(self.temp_directory, filename)
+
+    '''接下来需要为3个步骤中的每个都在整体上创建一个 "管理者" 方法, 职责是
+    将工作委托给其他方法'''
+    def zip_find_replace(self):
+        self.unzip_files()
+        self.find_replace()
+        self.zip_files()
+
+    def unzip_files(self):
+        os.mkdir(self.temp_directory)
+        zip = zipfile.ZipFile(self.filename)
+        try:
+            zip.extractall(self.temp_directory)
+        finally:
+            zip.close()
+
+    def find_replace(self):
+        for filename in os.listdir(self.temp_directory):
+            with open(self._full_filename(filename)) as file:
+                contents = file.read()
+            contents = contents.replace( self.search_string, self.replace_string)
+            with open(self._full_filename(filename), 'w') as file:
+                file.write(contents)
+
+    def zip_files(self):
+        file = zipfile.ZipFile(self.filename, 'w')
+        for filename in os.listdir(self.temp_directory):
+            file.write(self._full_filename(filename), filename)
+        shutil.rmtree(self.temp_directory)
+
+
+if __name__ == '__main__':
+    ZipReplace(*sys.argv[1:4]).zip_find_replace()
