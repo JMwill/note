39eeae0c837982286b3e3b9db29632cc578ea147
JMwill <787505998@qq.com>
2017-08-17 17:47:43 +0800

    修改 insert 为 select

--- Algorithmetic-summary/sort/test.js
+++ Algorithmetic-summary/sort/test.js
@@ -4,7 +4,7 @@ const data = require('./data');
 
 // 算法
 const bubbleSort = require('./bubble');
-const insertSort = require('./insert');
+const selectSort = require('./select');
 
 // 基准数据
 const randomWords = data.getRandomWords(1000);
@@ -14,8 +14,8 @@ const randomInts = data.getRandomInts(1000);
 const benchmarkOpt = {
     repeat: 100,
     times: 10,
-    logType: 'normal',
-    // logType: 'average',
+    // logType: 'normal',
+    logType: 'average',
 };
 
 // Bubble 排序的基准测试实例
@@ -24,14 +24,14 @@ let bubbleBenchmark = new Benchmark(function bubble() {
     bubbleSort(randomInts.slice());
 }, benchmarkOpt);
 
-let insertBenchmark = new Benchmark(function insert() {
-    insertSort(randomWords.slice());
-    insertSort(randomInts.slice());
+let selectBenchmark = new Benchmark(function select() {
+    selectSort(randomWords.slice());
+    selectSort(randomInts.slice());
 }, benchmarkOpt);
 
 function run() {
     bubbleBenchmark.run();
-    insertBenchmark.run();
+    selectBenchmark.run();
 }
 
 run();
