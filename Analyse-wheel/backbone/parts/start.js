ac823d11e166245c88e1c22ba47ee0f8a6f63971
JMwill <787505998@qq.com>
2017-11-15 23:19:16 +0800

    记录拆解的代码无意义，清除

--- Analyse-wheel/backbone/parts/start.js
+++
@@ -1,3 +0,0 @@
-// 根元素的确定
-self = (typeof self === 'object' && self.self === self && self) // 确定是否是浏览器下，用self是因为需要支持WebWorker
-    || (typeof global === 'object' && global.global === global && global) // 确定是否是node环境下
