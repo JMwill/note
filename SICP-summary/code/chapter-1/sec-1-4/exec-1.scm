423ff1116d1d67d38733a7f6cc8a7df6ee8a601c
Will <lijiaming@ifanr.com>
2017-11-30 23:44:08 +0800

    实现部分练习

--- SICP-summary/code/chapter-1/sec-1-4/exec-1.scm
+++ SICP-summary/code/chapter-1/sec-1-4/exec-1.scm
@@ -16,6 +16,8 @@
 ;;; (cond (<p1> <e1>)
 ;;;       (<p2> <e2>)
 ;;;       (<pn> <en>))
+;;; cond 还可以跟 else 搭配使用
+;;; 在最后加上 (else <e>)
 ;;;
 (define (abs x)
   (cond ((> x 0) x)
@@ -43,3 +45,42 @@
 (>= 10 10)
 
 ;;; Exercise 1.1
+10
+(+ 5 4 3)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4) (- 4 6))
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+(= a b)
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+(+ 2 (if (> b a) b a))
+(* (cond ((> a b) a)
+	 ((< a b) b)
+	 (else -1))
+   (+ a 1))
+
+;;; Exercise 1.2
+;;;
+(/ (+
+    5
+    (/ 1 2)
+    (- 2 (- 3 (+ 6 (/ 1 3)))))
+   (*
+    (- 2 6)
+    (- 2 7)))
+
+;;; Exercise 1.3
+;;;
+(define (sum-of-squares-of-two-larger-num x y z)
+  (cond ((and (< x y) (< x z)) (sum-of-squares z y))
+	((and (< y z) (< y x)) (sum-of-squares z x))
+	(else (sum-of-squares x y))))
+(sum-of-squares-of-two-larger-num 1 2 3)
+
