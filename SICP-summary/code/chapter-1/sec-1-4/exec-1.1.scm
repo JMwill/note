96911bd04b2424545745510619d656bd298d44bc
JMwill <787505998@qq.com>
2017-12-18 23:06:01 +0800

    修改练习

--- SICP-summary/code/chapter-1/sec-1-4/exec-1.1.scm
+++ SICP-summary/code/chapter-1/sec-1-4/exec-1.1.scm
@@ -197,3 +197,15 @@
 
 (cube-root 9)
 
+;;; 在过程内定义过程
+;;;
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+	guess
+	(sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
