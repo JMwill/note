f5a60470492500c624003efa821987a2e10cd7c3
JMwill <787505998@qq.com>
2018-05-12 20:45:42 +0800

    删除 rc 类型的文件，统一在 dotfiles 项目中管理

--- Linux-summary/ubuntu-summary/autoload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# open gnome-session-properties and add this script
-# command: 'bash /file/path' or 'gnome-terminal -x /file/path' to run this script
-gnome-terminal &
-google-chrome &
