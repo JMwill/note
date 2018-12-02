e5905206fa43b9f7e2e75efaca82b418ff40be88
JMwill <787505998@qq.com>
2018-12-02 22:10:40 +0800

    添加删除 .travis.yml 文件的代码

--- scripts/clean.js
+++ scripts/clean.js
@@ -4,16 +4,23 @@ const {log} = require('./utils')
 const config = require('./config')
 const {findAllReadmeSrc} = require('./build/readme')
 
+const specialType = ['input', 'output']
 async function main() {
   const type = process.argv[2]
+
   log('!!!!!! cleaning... !!!!!!')
-  const srcPathList = await findAllReadmeSrc(config.rootdir)
-  const createPromiseList = srcPathList.map(async src => {
-    const target = path.resolve(path.dirname(src), config.readme[type])
-    await fs.remove(target)
-    log(`${target} cleared`)
-  })
-  await Promise.all(createPromiseList)
+  if (!specialType.includes(type)) {
+    await fs.remove(path.resolve(__dirname, '..', type))
+    log(`${type} cleared`)
+  } else {
+    const srcPathList = await findAllReadmeSrc(config.rootdir)
+    const createPromiseList = srcPathList.map(async src => {
+      const target = path.resolve(path.dirname(src), config.readme[type])
+      await fs.remove(target)
+      log(`${target} cleared`)
+    })
+    await Promise.all(createPromiseList)
+  }
   log('!!!!!! finished !!!!!!')
 }
 
